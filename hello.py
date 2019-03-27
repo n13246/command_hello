c9ae4e44481015457974960b6c7e01eae739b831
n13246 <48742459+n13246@users.noreply.github.com>
2019-03-27 11:07:11 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
+
 print("Hello World!")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
-
+print("Tell my world")
